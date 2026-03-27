a9904148d01566cbe7299a608275edc8dbcc6d7a
Emmett Frett <emfrett@gmail.com>
2026-03-27 12:27:00 +0100

    fixes index file

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,7 +3,7 @@ export type User = {
   name: string;
 };
 
-export type KafeType = 'dublo' | 'kafe' | 'turkish kafe' | 'macchiato' | 'cappicino' | 'freddo' | 'other';
+export type KafeType = 'dublo' | 'kafe' | 'turkish kafe' | 'macchiato' | 'cappicino' | 'freddo' | 'cai' | 'other';
 
 export type KafeLog = {
   id: string;
