8d3240eed68f218a92152470dd7a09dd90a82799
Emmett Frett <emfrett@gmail.com>
2026-04-11 22:06:41 +0200

    maybe forces updates

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,6 @@ import './index.css'
 import { LanguageProvider } from './contexts/LanguageContext'
 import { registerSW } from 'virtual:pwa-register'
 
-<<<<<<< Updated upstream
-// Register the PWA service worker
-const updateSW = registerSW({
-  onNeedRefresh() {
-    // When you push new code to Vercel, this prompts the user's phone to grab the new version
-    if (confirm('A new update is available! Reload to apply?')) {
-      updateSW(true)
-    }
-=======
 // NEW: Force the browser to refresh the screen when a new Service Worker takes over
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.addEventListener('controllerchange', () => {
@@ -27,7 +18,6 @@ registerSW({
   onRegistered(r) {
     // NEW: Force the app to check Vercel for updates every time it is opened
     r && r.update();
->>>>>>> Stashed changes
   },
   onOfflineReady() {
     console.log('App is ready to work offline!')
